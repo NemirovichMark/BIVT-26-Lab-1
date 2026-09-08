2ec7ef695e7bd03f0458282118c9e75d299df4c9
Director0 <95127051+Director0@users.noreply.github.com>
2026-09-09 00:19:26 +0300

    blue: tasks: 1-7

--- Lab1/Blue.cs
+++ Lab1/Blue.cs
@@ -4,43 +4,59 @@ namespace Lab1
     {
         public bool Task1(int a, int b)
         {
+            // code here
             return (((a > 0) && (b > 0)) || ((a < 0) && (b < 0)));
+            // end
         }
         public bool Task2(double d)
         {
+            // code here
             return d % 1 != 0;
+            // end
         }
         public bool Task3(int a, int b)
         {
+            // code here
             return ((b != 0) && (a % b == 0));
+            // end
         }
         public double Task4(double d, double f, double g)
         {
-            double answer = 0;
-
-
-
-            return new[] { Math.Abs(d), Math.Abs(f), Math.Abs(g) }.Max();
+            List<double> nums = new List<double> { Math.Abs(d), Math.Abs(f), Math.Abs(g) };
+            // code here
+            if (Math.Abs(d) == nums.Max()) 
+            {
+                return d;
+            }
+            if (Math.Abs(f) == nums.Max())
+            {
+                return f;
+            }
+            return g;
+            // end
         }
         public double Task5(double x)
         {
-            double answer = 0;
 
             // code here
+            static double y(double x)
+            {
+                if (x <= -1) return 0;
+                if (x <= 0) return x + 1;
+                return 1;
 
+            }
             // end
 
-            return answer;
+            return y(x);
         }
         public bool Task6(double circleS, double squareS)
         {
             bool answer = false;
 
             // code here
-
+            return (2 * Math.Sqrt(circleS / Math.PI) <= Math.Sqrt(squareS));
             // end
-
-            return answer;
         }
 
         public double Task7(double d, double f)
@@ -48,7 +64,20 @@ namespace Lab1
             int answer = 0;
 
             // code here
-
+            if (Math.Abs(d) < Math.Abs(f))
+            {
+                if (d > 0)
+                {
+                    answer = -1;
+                }
+            }
+            else
+            {
+                if (f > 0)
+                {
+                    answer = 1;
+                }
+            }
             // end
 
             return answer;
@@ -58,7 +87,7 @@ namespace Lab1
             bool answer = false;
 
             // code here
-            
+            int bank = a / 2 + b / 2 + c / 2;
             // end
 
             return answer;
