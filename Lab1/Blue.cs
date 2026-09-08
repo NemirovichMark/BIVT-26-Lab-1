38f31e10c644a6c28e1c88c28936ffa31fef932c
vellonity <belousovi3252@gmail.com>
2026-09-08 17:37:31 +0300

    Update Blue.cs

--- Lab1/Blue.cs
+++ Lab1/Blue.cs
@@ -1,4 +1,4 @@
-﻿namespace Lab1
+namespace Lab1
 {
     public class Blue
     {
@@ -7,7 +7,12 @@
             bool answer = false;
 
             // code here
-
+            if ((a > 0 && b > 0) || (a < 0 && b <0)) {
+                    answer = true;
+            }
+            else {
+                    answer = false;
+            }
             // end
 
             return answer;
@@ -17,7 +22,9 @@
             bool answer = false;
 
             // code here
-
+            if {
+                answer = true;
+            }
             // end
 
             return answer;
