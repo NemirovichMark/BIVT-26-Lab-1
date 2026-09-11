be03554048049b04511703bea65f93165989e744
myDeveloper3 <coolproger777@gmail.com>
2026-09-11 18:58:04 +0300

    solution ready

--- Lab1/Purple.cs
+++ Lab1/Purple.cs
@@ -6,59 +6,79 @@ namespace Lab1
         {
             bool answer = false;
 
-            // code here
-
-            // end
-
+            if (a > 0 && b > 0 && c > 0 || a < 0 && b < 0 && c < 0)
+            {
+                answer = true;
+            }
             return answer;
         }
         public bool Task2(int a, int b)
         {
             bool answer = false;
 
-            // code here
-
-            // end
-
+            if (a != 0)
+            {
+                if (b % a == 0)
+                {
+                    answer = true;
+                }
+            } if (b != 0)
+            {
+                if (a % b == 0)
+                {
+                    answer = true;
+                }
+            }
             return answer;
         }
         public bool Task3(int a, int b)
         {
             bool answer = false;
-
-            // code here
-
-            // end
+            if (Math.Pow(a, 2) == b || Math.Pow(b, 2) == a)
+            {
+                answer = true;
+            }
+            if (Math.Pow(a, 3) == b || Math.Pow(b, 3) == a)
+            {
+                answer = true;
+            }
 
             return answer;
         }
         public double Task4(double d, double f, double g)
         {
             double answer = 0;
-
-            // code here
-
-            // end
+            // dx^2 + fx + g
+            answer = Math.Pow(f, 2) - 4 * d * g;
 
             return answer;
         }
         public double Task5(double x)
         {
             double answer = 0;
-
-            // code here
-
-            // end
+            if (x <= -1)
+            {
+                answer = 1;
+            } else if (x > -1 && x <= 1)
+            {
+                answer = -x;
+            } else
+            {
+                answer = -1;
+            }
 
             return answer;
         }
         public bool Task6(double squareS, double circleS)
         {
             bool answer = false;
-
-            // code here
-
-            // end
+            double side = Math.Sqrt(squareS);
+            double diag = Math.Sqrt(Math.Pow(side, 2) + Math.Pow(side, 2));
+            double radius = Math.Sqrt(circleS / Math.PI);
+            if (diag <= radius * 2)
+            {
+                answer = true;
+            }
 
             return answer;
         }
@@ -67,9 +87,37 @@ namespace Lab1
         {
             int answer = 0;
 
-            // code here
-
-            // end
+            if (s)
+            {
+                if (t)
+                {
+                    answer = 6;
+                } else
+                {
+                    if (f)
+                    {
+                        answer = 10;
+                    } else
+                    {
+                        answer = 2;
+                    }
+                }
+            } else
+            {
+                if (t)
+                {
+                    answer = 3;
+                } else
+                {
+                    if (f)
+                    {
+                        answer = 5;
+                    } else
+                    {
+                        answer = 1;
+                    }
+                }
+            }
 
             return answer;
         }
@@ -78,9 +126,19 @@ namespace Lab1
             bool answer = false;
             const int bank = 10000;
 
-            // code here
-            
-            // end
+            if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
+            {
+                answer = false;
+            } else
+            {
+                double aurors = Math.Ceiling(Convert.ToDouble(pupils) / 7);
+                double salary_aurors = aurors * salary;
+                int pupils_salary = pupils * 5;
+                if (salary_aurors + pupils_salary <= 10_000)
+                {
+                    answer = true;
+                }
+            }
 
             return answer;
         }
