bf2c581c91e04a3f6cc0a1c8195c031e15af2b3a
Yarkat <takraychik@gmail.com>
2026-09-14 13:54:20 +0300

    i did it all

--- Lab1/Purple.cs
+++ Lab1/Purple.cs
@@ -1,3 +1,5 @@
+using System.Numerics;
+
 namespace Lab1
 {
     public class Purple
@@ -7,7 +9,14 @@ namespace Lab1
             bool answer = false;
 
             // code here
-
+            if (a > 0 && b > 0 && c > 0)
+            {
+                answer = true;
+            }
+            else if (a < 0 && b < 0 && c < 0)
+            {
+                answer = true;
+            }
             // end
 
             return answer;
@@ -17,7 +26,10 @@ namespace Lab1
             bool answer = false;
 
             // code here
-
+            if (a%b == 0 || b % a == 0)
+            {
+                answer = true;
+            }
             // end
 
             return answer;
@@ -27,6 +39,20 @@ namespace Lab1
             bool answer = false;
 
             // code here
+            if (b > a)
+            {
+                if (a*a == b || a*a*a == b)
+                {
+                    answer = true;
+                }
+            }
+            if(a > b)
+            {
+                if (b * b == a || b * b * b == a)
+                {
+                    answer = true;
+                }
+            }
 
             // end
 
@@ -37,7 +63,7 @@ namespace Lab1
             double answer = 0;
 
             // code here
-
+            answer = f*f - 4 * d * g;
             // end
 
             return answer;
@@ -47,7 +73,18 @@ namespace Lab1
             double answer = 0;
 
             // code here
-
+            if (x <= -1)
+            {
+                answer = 1;
+            }
+            if (x > -1 && x <= 1)
+            {
+                answer = -x;
+            }
+            if (x > 1)
+            {
+                answer = -1;
+            }   
             // end
 
             return answer;
@@ -57,7 +94,12 @@ namespace Lab1
             bool answer = false;
 
             // code here
-
+            double a = Math.Sqrt(squareS);
+            double b = Math.Sqrt(circleS / Math.PI);
+            if (b * 2 <= a)
+            {
+                answer = true;
+            }
             // end
 
             return answer;
@@ -68,7 +110,42 @@ namespace Lab1
             int answer = 0;
 
             // code here
-
+            if (s == true)
+            {
+                if (t == true)
+                {
+                    answer = 6;
+                }
+                else
+                {
+                    if (f == true)
+                    {
+                        answer = 10;
+                    }
+                    else
+                    {
+                        answer = 2;
+                    }
+                }
+            }
+            else
+            {
+                if (t == true)
+                {
+                    answer = 3;
+                }
+                else
+                {
+                    if (f == true)
+                    {
+                        answer = 5;
+                    }
+                    else
+                    {
+                        answer = 1;
+                    }
+                }
+            }
             // end
 
             return answer;
@@ -79,7 +156,25 @@ namespace Lab1
             const int bank = 10000;
 
             // code here
-            
+            bool i1 = false;
+            bool i2 = false;
+            if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
+                i1 = false;
+            else
+            {   
+                i1 = true;
+            }
+            int cA = (int)Math.Ceiling((double)pupils / 7);
+            int tC = cA * salary + pupils * 5;
+
+            if (tC < bank)
+            {
+                i2 = true;
+            }
+            if (i1  == true && i2 == true)
+            {
+                answer = true;      
+            }
             // end
 
             return answer;
