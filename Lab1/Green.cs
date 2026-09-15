9389d55c799d4e3c710610e91bb3c9139cb14786
Artem3317 <artemzaichenko9@gmail.com>
2026-09-15 23:44:44 +0300

    1st lab done

--- Lab1/Green.cs
+++ Lab1/Green.cs
@@ -7,7 +7,9 @@
             bool answer = false;
 
             // code here
-
+            double absoluted = Math.Abs(d);
+            Console.WriteLine(absoluted);
+            if (absoluted >= 1) { answer = true; }
             // end
 
             return answer;
@@ -17,7 +19,8 @@
             bool answer = false;
 
             // code here
-
+            double avg = (d + f) / 2.0;
+            if (avg > 0.0) { answer = true; }
             // end
 
             return answer;
@@ -27,7 +30,9 @@
             bool answer = false;
 
             // code here
-
+            int sum = (a + b);
+            double avg = (Math.Abs(a) + Math.Abs(b) / 2.0);
+            if (sum > avg) { answer = true; }
             // end
 
             return answer;
@@ -37,7 +42,9 @@
             int answer = 0;
 
             // code here
-
+            if (a > b && a > c) { answer = a; }
+            if (b > a && b > c) { answer = b; }
+            if (c > a && c > b) { answer = c; }
             // end
 
             return answer;
@@ -47,7 +54,10 @@
             double answer = 0;
 
             // code here
-
+            double y;
+            if (Math.Abs(x) > 1) { y = 0; }
+            else { y = x * x - 1; }
+            return y;
             // end
 
             return answer;
@@ -57,7 +67,7 @@
             bool answer = false;
 
             // code here
-
+            if (y <= 1 - Math.Abs(x) && y >= 0) { answer = true; }
             // end
 
             return answer;
@@ -68,6 +78,8 @@
             bool answer = true;
 
             // code here
+            if (n < 0) { answer = false; }
+            else if (n % 2 == 0) { answer = false; }
 
             // end
 
@@ -78,7 +90,18 @@
             bool answer = false;
 
             // code here
+            int wakeup = 14 * 60;
+            int sleep = 4 * 60;
+            for (int day = 1; day <= X; day++)
+            {
+                if (wakeup > 7 * 60) wakeup -= 60;
 
+                if (day % 2 == 1)
+                    sleep -= Y;
+            }
+            int sleepDuration = (wakeup - sleep);
+            if (sleepDuration < 0) { sleepDuration += 24 * 60; }
+            if (wakeup == 7 * 60 && sleepDuration >= 7 * 60 && sleepDuration <= 9 * 60) { answer = true; }
             // end
 
             return answer;
