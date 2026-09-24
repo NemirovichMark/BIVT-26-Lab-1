7635a6f5753ef046443536f2e5928a9ce6745953
AlesNutt <alesya.orekhova.08@gmail.com>
2026-09-24 13:48:43 +0300

    Update Blue.cs

--- Lab1/Blue.cs
+++ Lab1/Blue.cs
@@ -22,8 +22,9 @@ namespace Lab1
             bool answer = false;
 
             // code here
+            d=Math.Abs(d);
             double c = d*100000;
-            if (c % 10000 > 0)
+            if (c % 100000 > 0)
             { answer = true; }
             // end
 
@@ -34,8 +35,12 @@ namespace Lab1
             bool answer = false;
 
             // code here
-            if (a%b==0)
-            { answer=true; }
+            if (b != 0)
+            {
+                if (a % b == 0) 
+                { answer = true; }
+            }
+
             // end
 
             return answer;
@@ -45,7 +50,18 @@ namespace Lab1
             double answer = 0;
 
             // code here
+            double da = Math.Abs(d);
+            double fa = Math.Abs(f);
+            double ga = Math.Abs(g);
 
+            if (da>=fa && da>=ga)
+            { answer = d; }
+
+            if(fa >= da && fa >= ga)
+            { answer = f; }
+
+            if(ga >= fa && ga >= da)
+            { answer = g; }
             // end
 
             return answer;
@@ -55,7 +71,12 @@ namespace Lab1
             double answer = 0;
 
             // code here
-
+            if (x<=-1)
+            {  answer = 0; }
+            if (x>-1 && x<= 0)
+            { answer = x+1; }
+            if (x>0)
+            { answer = 1; }
             // end
 
             return answer;
@@ -65,7 +86,10 @@ namespace Lab1
             bool answer = false;
 
             // code here
-
+            double Sc = Math.Sqrt(circleS / 3.14);
+            double Ss = Math.Sqrt(squareS);
+            if (Sc*2<=Ss)
+            { answer = true; }
             // end
 
             return answer;
@@ -76,7 +100,20 @@ namespace Lab1
             int answer = 0;
 
             // code here
-
+            if (Math.Abs(d)<Math.Abs(f))
+            {
+                if (d>0)
+                { answer = -1; }
+                else
+                { return answer; }
+            }
+            else
+            {
+                if (f>0)
+                { answer = 1; }
+                else
+                { return answer; }
+            }
             // end
 
             return answer;
@@ -86,10 +123,20 @@ namespace Lab1
             bool answer = false;
 
             // code here
-            
+            int[] money = {a,b,c};
+            int za = a / 2;
+            int zb = b / 2;
+            int zc = c / 2;
+            int mn = money.Min();
+            if (za + zb + zc >= 3 && (za+zb+zc)%3==0 && mn >= (za + zb + zc) / 3)
+            { answer=true; }
+            else if ((za+zb+zc)%3==2 && mn >= (za + zb + zc+1)/3)
+            { answer=true; }
+
             // end
 
             return answer;
         }
     }
 }
+
