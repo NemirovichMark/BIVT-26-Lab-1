a97ac346e0866327513ff5119ba1a36934496225
N0V0ST0RM <chell1cs@yandex.ru>
2026-09-26 09:22:37 +0300

    Update Blue.cs

--- Lab1/Blue.cs
+++ Lab1/Blue.cs
@@ -22,7 +22,9 @@
             // code here
             double fraction = Math.Abs(d - Math.Truncate(d));
             if (fraction >= 0.0001)
+            {
                 answer = true;
+            }
             // end
 
             return answer;
@@ -33,7 +35,9 @@
 
             // code here
             if (b != 0 && a % b == 0)
+            {
                 answer = true;
+            }
             // end
 
             return answer;
@@ -47,11 +51,17 @@
             double absf = Math.Abs(f);
             double absg = Math.Abs(g);
             if (absd > absf && absf >= absg)
+            {
                 answer = d;
+            }
             else if (absf > absd && absd >= absg)
+            {
                 answer = f;
+            }
             else
+            {
                 answer = g;
+            }
             // end
 
             return answer;
@@ -62,11 +72,17 @@
 
             // code here
             if (x <= -1)
+            {
                 answer = 0;
+            }
             else if (x > -1 && x <= 0)
+            {
                 answer = x + 1;
-            else
+            }
+            else if (x>0)
+            {
                 answer = 1;
+            }
             // end
 
             return answer;
@@ -77,7 +93,9 @@
 
             // code here
             if (circleS <= (Math.PI / 4) * squareS)
+            {
                 answer = true;
+            }
             // end
 
             return answer;
@@ -91,12 +109,16 @@
             if (Math.Abs(d) < Math.Abs(f))
             {
                 if (d > 0)
+                {
                     answer = -1;
+                }
             }
             else
             {
                 if (f > 0)
+                {
                     answer = 1;
+                }
             }
             // end
 
@@ -107,12 +129,13 @@
             bool answer = false;
 
             // code here
-            int s = (a / 2 + b / 2 + c / 2);
-            if (s % 3 == 0 && s / 3 >= 1 && s / 3 <= a && s / 3 <= b && s / 3 <= c)
-            {
-                answer = true;
-            }
-            else if ((s + 1) % 3 == 0 && (s + 1) / 3 >= 1 && s / 3 <= a && s / 3 <= b && s / 3 <= c)
+            int goldA = a / 2;
+            int goldB = b / 2;
+            int goldC = c / 2;
+            bool option1 = (goldA == goldB && goldB == goldC);
+            bool option2 = (goldA == goldB && goldB == (goldC + 1));
+            
+            if ((option1 || option2) && goldA >= 1 && goldB >= 1 && goldC >= 1)
             {
                 answer = true;
             }
